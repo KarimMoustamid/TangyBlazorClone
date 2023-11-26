23f02a65530d9f8ea857269ce2f61c955cc2c4b5
KarimMoustamid <karim.moustamid@agilethought.com>
2023-11-26 11:30:23 -0600

    two Way Data Binding

--- Tangy/Tangy_Models/LearnBlazorModels/Demo_Product.cs
+++ Tangy/Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -3,8 +3,9 @@
 public class Demo_Product
 {
     public int Id { get; set; }
-    
     public string Name { get; set; }
-
+    public double Price { get; set; }
     public bool IsActive { get; set; }
+    
+    public IEnumerable<Demo_ProductProp> ProductProperties { get; set; }
 }
